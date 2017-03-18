73fca3a8a5056987cd041a69db6468c273373633
Pol Llovet <pol.llovet@gmail.com>
2017-03-18 08:35:13 -0600

    adding a folder for scripts.

---
+++ scripts/mw-to-md.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+if [ -z "$1" ]; then
+  echo "USAGE: $0 <folder with mediawiki files>"
+  exit -1
+fi
+
+if ! which pandoc 2> /dev/null; then
+  echo "Script requires pandoc to be installed and in the path."
+  exit -1
+fi
+
+
+for i in mediawiki/*.mw; do
+  echo "Processing $i..."
+  cat $i | \
+  sed 's/class = "/class="/g' | \
+  pandoc -o $i.md -f mediawiki -t markdown
+done
